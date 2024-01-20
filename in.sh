6323c3ecc85a801865396723d20f8bceaa1469ba
Your Name <you@example.com>
2024-01-20 19:39:16 +0200

    new

--- in.sh
+++ in.sh
@@ -1,2 +1,8 @@
+install(){
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
+}
 sudo nixos-rebuild --switch --flake .#system
+
 #home-manager switch --flake .#user
