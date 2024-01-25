768de794334690cbd660ad41cec17d62a32204cd
Ernestas Damanskis <ernestasdamanskis@gmail.com>
2024-01-25 16:50:23 +0200

    the usual

--- pkgs/color.nix
+++ pkgs/color.nix
@@ -4,7 +4,9 @@ imports = [
 inputs.nix-colors.homeManagerModules.default
 ];
 # colorScheme = inputs.nix-colors.colorSchemes.gruvbox-dark-medium;
- colorScheme = inputs.nix-colors.colorSchemes.tokyo-night-dark;
+ # colorScheme = inputs.nix-colors.colorSchemes.tokyo-night-dark;
+
+ colorScheme = inputs.nix-colors.colorSchemes.tokyo-night-storm;
 	
 
 
--- users/chad/home.nix
+++ users/chad/home.nix
@@ -26,6 +26,7 @@
 			../../pkgs/wlsunset.nix
 			../../pkgs/mpv.nix
 			../../pkgs/cursor.nix
+			../../pkgs/transmission.nix
 			../../pkgs/firefox.nix
 		] ;
 # The home.packages option allows you to install Nix packages into your
