05a20062c9f4c4d9485c58da8c72feb5281333a9
Ernestas Damanskis <ernestasdamanskis@gmail.com>
2024-01-26 17:37:32 +0200

    the usual

--- pkgs/transmission.nix
+++ pkgs/transmission.nix
@@ -7,6 +7,9 @@ services.transmission = {
     settings = { #Override default settings
       rpc-bind-address = "0.0.0.0"; #Bind to own IP
       rpc-whitelist = "127.0.0.1,10.0.0.1"; #Whitelist your remote machine (10.0.0.1 in this example)
+    download-dir = "/home/chad/Downloads";
+    # script-torrent-done-enabled = true;
+    # script-torrent-done-filename = "/home/chad/.bin/tordone";
     };
   };
 
