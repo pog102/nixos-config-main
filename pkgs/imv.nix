b122c614620eae60ea5d1e65f3cc5c47658d38fe
Ernestas Damanskis <ernestasdamanskis@gmail.com>
2024-02-14 22:32:01 +0200

    the usual

--- pkgs/imv.nix
+++ pkgs/imv.nix
@@ -2,8 +2,9 @@
 let 
 rename = pkgs.pkgs.writeShellScript "rename" ''
 #!/bin/sh
-new="$(rofi -dmenu -l 1)"
-[ ! -z $new ] && mv $1 "$(dirname $1)/$new.png" 
+ new="$(rofi -dmenu -l 1)"
+ [ ! -z $new ] && mv $1 "$(dirname $1)/$new.png" 
+exec /nix/store/mbn8520af7wsj7dydih90cbj6lpa8gwq-user-environment/bin/notify-send -r 188 "$1"
 '';
 in
 {
@@ -15,8 +16,15 @@ settings ={
   q = "quit";
 gg = "goto 1";
 "<Shift+G>" = "goto -1";
+
+};
+binds = {
+
 x = "exec rm \"$imv_current_file\"; close";
 u = "exec ${rename} \"$imv_current_file\"";
+y = "exec /nix/store/mbn8520af7wsj7dydih90cbj6lpa8gwq-user-environment/bin/notify-send -r 188 \"Copied\" \"\" && wl-copy < \"$imv_current_file\"";
+
+
 };
 };
 };
