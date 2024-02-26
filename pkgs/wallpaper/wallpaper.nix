4ac365776a81d747221b960a81f299a9847a9717
Ernestas Damanskis <ernestasdamanskis@gmail.com>
2024-02-26 14:22:58 +0200

    Added some stuff

--- pkgs/wallpaper/wallpaper.nix
+++ pkgs/wallpaper/wallpaper.nix
@@ -1,5 +1,11 @@
  { pkgs,userSettings, ... }:
- {
+{
+
+	home.file."Pictures/town.png".source = pkgs.fetchurl {
+	url = "https://w.wallhaven.cc/full/d6/wallhaven-d6mg33.png";
+	sha256 ="sha256-fYMzoY3un4qGOSR4DMqVUAFmGGil+wUze31rLLrjcAc=";
+	# sha256 =pkgs.lib.fakeHash;
+	};
 
 	home.file."Pictures" = {
 		recursive = true;
