02f4ad7dc6e7407e1c708d6eb4196d95738c7d56
Ernestas Damanskis <ernestasdamanskis@gmail.com>
2024-02-27 17:14:54 +0200

    Added some stuff

--- pkgs/lf.nix
+++ pkgs/lf.nix
@@ -1,52 +1,52 @@
 { config, pkgs, ... }:
 {
   xdg.configFile."lf/icons".source = ../icons;
-programs.lf = {
-enable=true;
-commands = {
-editor-open = ''$$EDITOR $f'';
-mkdir = ''
-''${{
-	clear
-printf "mkdir: "
-read DIR
-mkdir $DIR
-  lf -remote "send $id select $DIR"
-}}
-'';
-open = ''
-''${{
-    case $(file --mime-type -Lb $f) in
-        text*|inode*) lf -remote "send $id \$$EDITOR \$fx";;
-        *) for f in $fx; do $OPENER $f > /dev/null 2> /dev/null & done;;
-    esac
-}}
+  programs.lf = {
+    enable = true;
+    commands = {
+      editor-open = ''$$EDITOR $f'';
+      mkdir = ''
+        ''${{
+        	clear
+        printf "mkdir: "
+        read DIR
+        mkdir $DIR
+          lf -remote "send $id select $DIR"
+        }}
+      '';
+      open = ''
+        ''${{
+            case $(file --mime-type -Lb $f) in
+                text*|inode*) lf -remote "send $id \$$EDITOR \$fx";;
+                *) for f in $fx; do $OPENER $f > /dev/null 2> /dev/null & done;;
+            esac
+        }}
 
 
-'';
-create = ''
-''${{
-clear
-  printf "File Name: "
-  read ans
-  case $ans in
-   *nix)
-	cp ~/.config/nvim/templates/skel.nix $ans
-      ;;
-   *sh)
-	cp ~/.config/nvim/templates/skeleton.sh $ans
-      ;;
-   *)
-  	touch $ans
-     ;;
-esac
+      '';
+      create = ''
+        ''${{
+        clear
+          printf "File Name: "
+          read ans
+          case $ans in
+           *nix)
+        	cp ~/.config/nvim/templates/skel.nix $ans
+              ;;
+           *sh)
+        	cp ~/.config/nvim/templates/skeleton.sh $ans
+              ;;
+           *)
+          	touch $ans
+             ;;
+        esac
 
-  lf -remote "send $id select $ans"
-  # lf -remote "send $id open"
-}}
-'';
-};
- keybindings = {
+          lf -remote "send $id select $ans"
+          # lf -remote "send $id open"
+        }}
+      '';
+    };
+    keybindings = {
 
       "\\\"" = "";
       o = "";
@@ -56,9 +56,9 @@ esac
       "`" = "mark-load";
       "\\'" = "mark-load";
       "<enter>" = "open";
-      
+      f = "filter";
       do = "dragon-out";
-      D="delete";
+      D = "delete";
 
       "g~" = "cd";
       gh = "cd";
@@ -66,43 +66,53 @@ esac
 
       ee = "editor-open";
       V = ''$${pkgs.bat}/bin/bat --paging=always --theme=gruvbox "$f"'';
-	r=":rename; cmd-delete-home"	;
+      r = ":rename; cmd-delete-home";
       # ...
-      extraConfig = 
-    let 
-      previewer = 
-        pkgs.writeShellScriptBin "pv.sh" ''
-case "$(file -Lb --mime-type -- "$1")" in
-    image/*)
-        # chafa -f sixel -s "$2x$3" --animate off --polite on "$1"
-        chafa -s "$2x$3" --animate off --polite on "$1"
-	# notify-send "$1"
-        # chafa -f sixel --animate off --polite on "$1"
-        exit 1
-        ;;
-	*)
-				bat --color=always --style=plain --pager=never "$1"
-		;;
-	esac
+      extraConfig =
+        let
+          previewer =
+            pkgs.writeShellScriptBin "pv.sh" ''
+              case "$(file -Lb --mime-type -- "$1")" in
+                  image/*)
+                      # chafa -f sixel -s "$2x$3" --animate off --polite on "$1"
+                      chafa -s "$2x$3" --animate off --polite on "$1"
+              	# notify-send "$1"
+                      # chafa -f sixel --animate off --polite on "$1"
+                      exit 1
+                      ;;
+              	*)
+              				bat --color=always --style=plain --pager=never "$1"
+              		;;
+              	esac
 
-      '';
-      cleaner = pkgs.writeShellScriptBin "clean.sh" ''
-        ${pkgs.chafa}/bin/chafa --clear  < /dev/null > /dev/tty
-      '';
-    in
-    ''
-      # set cleaner ${cleaner}/bin/clean.sh
-      set previewer ${previewer}/bin/pv.sh
-    '';
+            '';
+          cleaner = pkgs.writeShellScriptBin "clean.sh" ''
+            ${pkgs.chafa}/bin/chafa --clear  < /dev/null > /dev/tty
+          '';
+        in
+        ''
+          # set cleaner ${cleaner}/bin/clean.sh
+          set previewer ${previewer}/bin/pv.sh
+        '';
     };
     settings = {
       preview = true;
       hidden = true;
       drawbox = true;
-      sixel=true;
+      sixel = true;
       icons = true;
       ignorecase = true;
+      incfilter = true;
+      promptfmt = ''
+        \033[48;2;35;38;39;38;2;28;220;156m 󱄅
+        \033[38;2;35;38;39;48;2;202;31;31m
+        \033[38;2;202;31;31;48;2;40;47;62m
+        \033[38;2;255;255;255m %w 
+        \033[38;2;40;47;62;48;2;58;67;89m
+        \033[38;2;255;255;255m %f 
+        \033[;38;2;58;67;89;49m\033[m"
+      '';
     };
-};
+  };
 }
 
