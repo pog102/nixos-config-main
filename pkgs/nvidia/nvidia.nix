8e8051d37d5ad619504dc44e634c98c7e7df2536
Ernestas Damanskis <ernestasdamanskis@gmail.com>
2024-03-02 13:14:52 +0200

    03-02 13:14

--- pkgs/nvidia/nvidia.nix
+++ pkgs/nvidia/nvidia.nix
@@ -40,7 +40,7 @@ prime = {
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
+    open = true;
     # open = false;
 
     # Enable the Nvidia settings menu,
