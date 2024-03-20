8ade0bcdbb22b656fde2202aa2b17c87f6044b98
Ernestas Damanskis <ernestasdamanskis@gmail.com>
2024-03-20 19:45:43 +0200

    03-20 19:45

--- pkgs/nyaa/default.nix
+++ pkgs/nyaa/default.nix
@@ -1,5 +1,4 @@
-{ pkgs,lib, fetchFromGitHub, rustPlatform, ... }:
-{
+{ lib, fetchFromGitHub, rustPlatform, pkgs ? import <nixpkgs> { } }:
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "nyaa";
   version = "v0.6.3";
@@ -8,10 +7,10 @@ pkgs.rustPlatform.buildRustPackage rec {
     owner = "Beastwick18";
     repo = pname;
     rev = version;
-    hash =lib.fakeHash;
+    hash = "sha256-wq4oD6GV7xi58Y6WHcnfUg9hV3ClSS5NF4tRyrviveE=" ;
   };
 
-    cargoHash = lib.fakeHash;
+    cargoHash = "sha256-2NwrMrQ/hYObU3aICgwn5+D0tm+aBImMi+7Fz0QHlso=";
 
   meta = {
     description = "A fast line-oriented regex search tool, similar to ag and ack";
@@ -19,5 +18,4 @@ pkgs.rustPlatform.buildRustPackage rec {
     license = lib.licenses.unlicense;
     maintainers = [];
   };
-};
 }
