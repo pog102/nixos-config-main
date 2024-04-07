2e465a1ae53e7c3ed1d70ef5acf2beff7e8864ff
Ernestas Damanskis <ernestasdamanskis@gmail.com>
2024-04-07 15:21:02 +0300

    04-07 15:21

--- homeManagerModules/nyaa/default.nix
+++ homeManagerModules/nyaa/default.nix
@@ -7,15 +7,19 @@ pkgs.rustPlatform.buildRustPackage rec {
     owner = "Beastwick18";
     repo = pname;
     rev = version;
-    hash = "sha256-h2jpZ4l627g3L5lwUwhcQnRVqSHQVMfnQrkoCJ9n2Gk=" ;
+    # hash = "sha256-h2jpZ4l627g3L5lwUwhcQnRVqSHQVMfnQrkoCJ9n2Gk=" ;
+    hash = "sha256-SVL81RSrc3HGVkSghlQUvZzqZctfwWFSENkPWCo6upA=";
+
   };
 
-    cargoHash = "sha256-gU1hzzGV/Gdg6LnHRzZlGfSwxG28bXw/PTx5xoaW+RQ=";
+  # cargoHash = "sha256-gU1hzzGV/Gdg6LnHRzZlGfSwxG28bXw/PTx5xoaW+RQ=";
+  cargoHash = "sha256-XuzfQJCAGZ/wx4jegRK0DG031n3Y52VOCDabaj5Axno=";
+
 
   meta = {
     description = "A fast line-oriented regex search tool, similar to ag and ack";
     homepage = "https://github.com/Beastwick18/nyaa";
     license = lib.licenses.unlicense;
-    maintainers = [];
+    maintainers = [ ];
   };
 }
