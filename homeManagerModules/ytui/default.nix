f24223e0749ef0e68b82600fbb9833732e833fe0
Ernestas Damanskis <ernestasdamanskis@gmail.com>
2024-04-26 14:36:40 +0300

    04-26 14:36

--- homeManagerModules/ytui/default.nix
+++ homeManagerModules/ytui/default.nix
@@ -1,23 +1,19 @@
 { lib, fetchFromGitHub, rustPlatform, pkgs ? import <nixpkgs> { } }:
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "ytui-music";
-  version = "main";
+  # version = "main";
+  version = "v2.0.0-rc1";
+  # version = "0x00007f0bd4c9b558";
+  # version = "refs/tags/main.zip";
 
   src = fetchFromGitHub {
     owner = "sudipghimire533";
     repo = pname;
     rev = version;
-    hash = "";
+    hash = "sha256-f/23PVk4bpUCvcQ25iNI/UVXqiPBzPKWq6OohVF41p8=";
   };
+  nativeBuildInputs = [ pkgs.pkg-config ];
+  buildInputs = [ pkgs.openssl ];
+  cargoHash = "sha256-ckpqq2XK/OhKWspL55/h0ZpwPZb3iQtoTUEoKNTjap8=";
 
-  #     nativeBuildInputs = [ pkgs.pkg-config ];
-  # buildInputs = [ pkgs.openssl ];
-  cargoHash = "";
-
-  meta = {
-    description = "A fast line-oriented regex search tool, similar to ag and ack";
-    homepage = "https://github.com/Beastwick18/nyaa";
-    license = lib.licenses.unlicense;
-    maintainers = [ ];
-  };
 }
